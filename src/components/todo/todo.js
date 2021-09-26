794809c1948d63e390df949479ab4f35551b3cab
Tariq Abulaban <tariqabulaban@hotmail.com>
2021-09-26 19:05:13 +0300

    finish_lab31

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -1,13 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import useForm from '../../hooks/form.js';
-
 import { v4 as uuid } from 'uuid';
 
+import Header from '../header/header.js';
+import Form from '../form/form.js';
+import List from '../list/list.js';
+import { SettingsContext } from '../../context/settings';
+
+
 const ToDo = () => {
+
+  const settings = useContext(SettingsContext);
 
   const [list, setList] = useState([]);
   const [incomplete, setIncomplete] = useState([]);
   const { handleChange, handleSubmit } = useForm(addItem);
+
+  const [startPage, setStartPage] = useState(0);
+  const [endPage, setEndPage] = useState(settings.itemNumber);
 
   function addItem(item) {
     console.log(item);
@@ -17,15 +27,15 @@ const ToDo = () => {
   }
 
   function deleteItem(id) {
-    const items = list.filter( item => item.id !== id );
+    const items = list.filter(item => item.id !== id);
     setList(items);
   }
 
   function toggleComplete(id) {
 
-    const items = list.map( item => {
-      if ( item.id == id ) {
-        item.complete = ! item.complete;
+    const items = list.map(item => {
+      if (item.id == id) {
+        item.complete = !item.complete;
       }
       return item;
     });
@@ -40,45 +50,37 @@ const ToDo = () => {
     document.title = `To Do List: ${incomplete}`;
   }, [list]);
 
+  function pagination() {
+    let result = list.slice(startPage, endPage);
+    return result;
+  }
+
+  function next() {
+    setStartPage(startPage + settings.itemNumber);
+    setEndPage(endPage + settings.itemNumber);
+  }
+  function previous() {
+    setEndPage(endPage - settings.itemNumber);
+    setStartPage(startPage - settings.itemNumber);
+  }
+
   return (
     <>
-      <header>
-        <h1>To Do List: {incomplete} items pending</h1>
-      </header>
+      <Header incomplete={incomplete} />
 
-      <form onSubmit={handleSubmit}>
 
-        <h2>Add To Do Item</h2>
+      <Form
+        handleSubmit={handleSubmit}
+        handleChange={handleChange}
+      />
 
-        <label>
-          <span>To Do Item</span>
-          <input onChange={handleChange} name="text" type="text" placeholder="Item Details" />
-        </label>
-
-        <label>
-          <span>Assigned To</span>
-          <input onChange={handleChange} name="assignee" type="text" placeholder="Assignee Name" />
-        </label>
-
-        <label>
-          <span>Difficulty</span>
-          <input onChange={handleChange} defaultValue={3} type="range" min={1} max={5} name="difficulty" />
-        </label>
-
-        <label>
-          <button type="submit">Add Item</button>
-        </label>
-      </form>
-
-      {list.map(item => (
-        <div key={item.id}>
-          <p>{item.text}</p>
-          <p><small>Assigned to: {item.assignee}</small></p>
-          <p><small>Difficulty: {item.difficulty}</small></p>
-          <div onClick={() => toggleComplete(item.id)}>Complete: {item.complete.toString()}</div>
-          <hr />
-        </div>
-      ))}
+      <List
+        pagination={pagination}
+        next={next}
+        previous={previous}
+        toggleComplete={toggleComplete}
+        deleteItem={deleteItem}
+      />
 
     </>
   );
